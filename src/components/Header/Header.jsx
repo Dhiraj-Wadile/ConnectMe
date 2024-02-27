001bbf752eb09a95ac0e9da7391f2a8850340923
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2024-02-27 17:10:18 +0530

    First Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+import css from "./Header.module.scss";
+import { BiPhoneCall, BiMenuAltRight } from "react-icons/bi";
+import { motion } from "framer-motion";
+import { getMenuStyles, headerVariants } from "../../utils/motion";
+import useOutsideAlerter from "../../hooks/useOutsideAlerter";
+import useHeaderShadow from "../../hooks/useHeaderShadow";
+
+const Header = () => {
+  const menuRef = useRef(null);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerShadow = useHeaderShadow();
+
+  //to handle click outside of sidebar on mobile
+  useOutsideAlerter({
+    menuRef,
+    setMenuOpened,
+  });
+
+  return (
+    <motion.div
+      variants={headerVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`bg-primary paddings ${css.wrapper}`}
+      viewport={{ once: true, amount: 0.25 }}
+      style={{boxShadow: headerShadow}}
+    >
+      <div className={`innerWidth ${css.container} flexCenter`}>
+      <div
+  className={css.name}
+  style={{
+    fontSize: '2em',
+    fontWeight: 'bold',
+    color: '#000',
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)',
+    letterSpacing: '1px',
+    animation: 'opacity: 0; transform: translateY(20px); animation: fadeInUp 1s ease-in-out forwards;',
+  }}
+>
+  ConnectMe
+</div>
+
+
+
+        <ul
+          className={`flexCenter ${css.menu}`}
+          ref={menuRef}
+          style={getMenuStyles(menuOpened)}
+        >
+          <li><a href="#experties">Services</a></li>
+          <li><a href="#work">Experience</a></li>
+          <li><a href="#portfolio">Projects</a></li>
+          <li><a href="#people">Testimonials</a></li>
+          <li className={`flexCenter ${css.phone}`}>
+            <p>+91 8767766059</p>
+            <BiPhoneCall size={"40px"} />
+          </li>
+        </ul>
+
+        {/* for medium and small screens */}
+        <div
+          className={css.menuIcon}
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Header;
---
+++ src/utils/data.js
@@ -0,0 +1,152 @@
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { CiMobile1 } from "react-icons/ci";
+import { MdWorkspacesOutline } from "react-icons/md";
+export const projectExperience = [
+  {
+    name: "Website Design",
+    projects: 9,
+    icon: HiOutlineDesktopComputer,
+    bg: "#286F6C",
+  },
+  {
+    name: "Mobile App Design",
+    projects: 2,
+    icon: CiMobile1,
+    bg: "#EEC048",
+  },
+ 
+];
+
+export const WhatDoIHelp = [
+  "Empowering Solutions: Let me guide you towards resolution, addressing and solving your challenges.",
+  "Navigating Challenges: Offering assistance in finding solutions and resolving issues effectively together.",
+];
+
+export const workExp = [
+  {
+    place: "Brainwave Learining Library", 
+    tenure: "Jan 2024 - Mar 2024",
+    role: "Frontend Developer",
+    detail:
+      "Designed an inclusive Bihar student platform, integrating diverse activities through intuitive ReactJS.",
+  },
+
+  {
+    place: "Coding Ninjas",
+    tenure: "Aug 2023 - Nov 2023",
+    role: "TA - C++ and DataStructures & Algorithms", 
+    detail:
+    "Conducted C++ and DSA doubt sessions, excelling in debugging, while fostering strong software development foundations and optimizing diverse projects with expertise.",
+  },
+  {
+    place: "WebStack Academy",
+    tenure: "Aug 2023 - Oct 2023",
+    role: "MERN Stack Web Developer",
+    detail:
+      "Completed end-to-end development for a food ordering app, utilizing MongoDB and Node.js, showcasing MERN stack expertise.",
+  },
+];
+
+export const comments = [
+  {
+    name: "ABC",
+    post: "HR",
+    comment:
+      "With ConnectMe, my website achieved its finest form, and I'm thankful for its seamless functionality; highly recommend it to others.",
+    img: "./people2.png",
+  },
+  {
+    name: "Dhiraj Wadile",
+    post: "Web Developer",
+    comment:
+      "Using ConnectMe, I obtained the best version of my website, grateful for its proper usage, and wholeheartedly recommend it to others.",
+    img: "./people1.png",
+  },
+  {
+    name: "XYZ",
+    post: "Software Developer",
+    comment:
+    "ConnectMe delivered the optimal version of my website, and I'm grateful for its effectiveness; I recommend it wholeheartedly to others.",
+    img: "./people2.png",
+  },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people1.png",
+  // },
+  // {
+  //   name: "Anamika Sandula",
+  //   post: "Creative Manager",
+  //   comment:
+  //     "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //   img: "./people2.png",
+  // },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
